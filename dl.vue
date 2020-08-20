1a1b58e2b5e72bc467ddefc27e3d1d3a2811aebe
My-Value <67990679+My-Value@users.noreply.github.com>
2020-08-20 09:00:56 +0800

    Add files via upload

---
+++ dl.vue
@@ -0,0 +1,28 @@
+<template>
+ <div>
+      <h1>
+      这是登录的界面
+  </h1>
+  <button class="loginbutton" @click="login()">登录</button>
+ </div>
+</template>
+
+<script>
+import {Loging,userLogin} from '@/service/login.js'
+
+export default{
+    methods:{
+        login:function(){
+            alert(userLogin);
+            Loging();
+            alert(userLogin);
+        }
+    }
+}
+</script>
+
+<style>
+.loginbutton{
+    margin: auto;
+}
+</style>
